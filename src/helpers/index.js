19ce39598a6687b4e5276c9ed39e6ca766f1eed8
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-09-11 22:42:53 +0700

    add validation method

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,9 @@
 import helperScaling from './scaling';
 import helperLanguage from './language';
 import helperSocmed from './socmed';
+import helperValidation from './validation';
 
 export const scaling = helperScaling;
 export const language = helperLanguage;
 export const socmed = helperSocmed;
+export const validation = helperValidation;
