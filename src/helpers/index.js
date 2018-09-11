95851b74dff66ea7517b86a65fe53f6cca0bad0d
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-09-11 22:27:23 +0700

    integrate facebook and google signin

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,7 @@
 import helperScaling from './scaling';
 import helperLanguage from './language';
+import helperSocmed from './socmed';
 
 export const scaling = helperScaling;
 export const language = helperLanguage;
+export const socmed = helperSocmed;
