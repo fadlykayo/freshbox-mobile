dbaee169277e7155a6688492c5d563ba3dbc82cd
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-09-11 18:45:02 +0700

    update StaticText renderText method

--- src/components/StaticText/index.js
+++ src/components/StaticText/index.js
@@ -1,6 +1,6 @@
 import React,{ PureComponent } from 'react';
 import { Text } from 'react-native';
-import lang from '@languages';
+import { language } from '@helpers';
 
 class StaticText extends PureComponent {
     constructor(){
@@ -15,68 +15,11 @@ class StaticText extends PureComponent {
         this.renderText(this.props.property,this.props.language,this.props.params);
     }
 
-    renderText(property,language = 'english',params = {}){
-
-        let propertyArray = property.trim().split('.');
-
-        if(lang.translations[language]){
-
-            let langObj = lang.translations[language];
-
-            for(i=0; i<propertyArray.length; i++){
-
-                if(langObj[propertyArray[i]]){
-
-                    langObj = langObj[propertyArray[i]];
-
-                } else {
-
-                    langObj = property;
-                    
-                    break;
-
-                }
-
-            }
-
-            if(langObj !== property){
-
-                let stringOutput = langObj;
-                let keyArray = Object.keys(params);
-
-                if(keyArray.length > 0){
-
-                    keyArray.map((e) => {
-
-                        let regex = new RegExp(`{${e}}`,'g');
-
-                        stringOutput = stringOutput.replace(regex,params[e]);
-                        
-                    });
-
-                    this.setState({outputText: stringOutput});
-                    // return stringOutput;
-
-                } else {
-
-                    this.setState({outputText: stringOutput});
-                    // return stringOutput;
-                
-                };
-
-            } else {
-
-                this.setState({outputText: `${property} not found`});
-                // return `${props} not found`;
-            
-            };
-
-        } else {
-
-            this.setState({outputText: `error: missing language ${language}`});
-            // return 'error: missing language';
-        
-        };
+    renderText(property = 'no_props',lang = 'english',params = {}){
+        language.transformText(property,lang,params)
+        .then((res) => {
+            this.setState({outputText: res});
+        });
     }
 
     render(){
