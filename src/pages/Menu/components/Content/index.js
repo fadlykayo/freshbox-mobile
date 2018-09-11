568f42bf033f8e2e6f925556f5c0c8ce967af78e
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-09-11 18:47:04 +0700

    add onPress method at Button

--- src/pages/Menu/components/Content/index.js
+++ src/pages/Menu/components/Content/index.js
@@ -14,15 +14,19 @@ class Content extends PureComponent {
             <View style={styles.container}>
                 <Button 
                     type={'get_started'}
+                    onPress={this.props.getStartedHandler}
                 />
                 <Button 
                     type={'facebook'}
+                    onPress={this.props.facebookHandler}
                 />
                 <Button 
                     type={'email'}
+                    onPress={this.props.emailHandler}
                 />
                 <Button 
                     type={'google'}
+                    onPress={this.props.googleHandler}
                 />
                 <StaticText 
                     style={styles.termsAndCondition}
