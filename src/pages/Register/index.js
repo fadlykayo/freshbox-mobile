339a1c108822384106fa2cabde41d38f77960f93
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-09-17 05:33:36 +0700

    update component

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import {  ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import { actNav, navConstant } from '@navigations';
 import { validation } from '@helpers';
 import Container from '@components/Container';
