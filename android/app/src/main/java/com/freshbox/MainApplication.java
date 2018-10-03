a2ab4bbfe62487bac60145adcdc5a8690b80296d
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-10-03 11:50:53 +0700

    add package image picker

--- android/app/src/main/java/com/freshbox/MainApplication.java
+++ android/app/src/main/java/com/freshbox/MainApplication.java
@@ -3,6 +3,7 @@ package com.freshbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new RNGoogleSigninPackage(),
           new FBSDKPackage(mCallbackManager),
           new VectorIconsPackage()
