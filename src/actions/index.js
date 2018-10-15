0983f36d952da4c7ddc7d3594ec7db52a07b09ff
Arfanizar Fathurochman <arfanizar@rebelworks.co>
2018-10-15 12:29:36 +0700

    add network action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,12 +2,14 @@ import registration from './registration';
 import auth from './auth';
 import product from './product';
 import user from './user';
+import network from './network';
 
 const action = {
     registration: registration,
     auth: auth,
     product: product,
     user: user,
+    network: network,
 }
 
 export default action;
