f70ac63619a2916aa3d5faa6a7cdea11c18b0276
rizkyanas35 <rizky.bukhori@rebelworks.co>
2020-12-01 15:36:28 +0700

    fixing error

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -1,43 +1,36 @@
 // import Analytics from 'appcenter-analytics';
-import firebase from 'react-native-firebase';
-import { store } from '../store';
-
+import firebase from '@react-native-firebase/app';
+import {store} from '../store';
 
 const Analytics = firebase.analytics();
 const analysis = {};
 
 analysis.init = () => {
   Analytics.setAnalyticsCollectionEnabled(true);
-}
+};
 
 analysis.logEvent = (eventName) => {
-
   let payload = store.getState().product.cart;
 
   switch (eventName) {
     case 'Cart':
-
       Analytics.logEvent('add_to_cart', {cart_items: payload});
       break;
     case 'Checkout':
-      
       Analytics.logEvent('begin_checkout', {cart_items: payload});
     case 'ChoosePayment':
-
       Analytics.logEvent('ecommerce_purchase', {cart_items: payload});
     default:
       break;
   }
+};
 
-  
-}
-
-analysis.log = (type,payload) => {
+analysis.log = (type, payload) => {
   Analytics.logEvent(type, payload);
-}
+};
 
 analysis.setCurrentScreen = (screenName) => {
   Analytics.setCurrentScreen(screenName, screenName);
-}
+};
 
-export default analysis
+export default analysis;
