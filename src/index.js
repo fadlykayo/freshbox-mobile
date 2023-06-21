52db5886d6799fca106a587b5f85fd80b03afd6d
Bhakti Budiman Novanda <myforework@live.com>
2023-06-21 11:59:20 +0700

    one signal version updated to 4.5.1, used OneSignal.setAppId instead of init, and add notif handler

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+import {AppContainer, actNav, navConstant, setNavigator} from '@navigations';
+import {AppState, Linking, StatusBar, View} from 'react-native';
 import React, {Component} from 'react';
-import {StatusBar, View, AppState, Linking} from 'react-native';
-import {connect} from 'react-redux';
+
 import OneSignal from 'react-native-onesignal';
-import {AppContainer, setNavigator, actNav, navConstant} from '@navigations';
 import actions from '@actions';
+import {connect} from 'react-redux';
 
 const mapStateToProps = (state) => ({
   user: state.user,
   product: state.product.products,
-  promoProduct: state.product.promoProduct.filter(x => !x.isClaim && x),
+  promoProduct: state.product.promoProduct.filter((x) => !x.isClaim && x),
   setModalVisible: state.product.setModalVisible,
 });
 
@@ -34,7 +35,7 @@ class App extends Component {
     this.onReceived = this.onReceived.bind(this);
     this.onOpened = this.onOpened.bind(this);
     this.onIds = this.onIds.bind(this);
-    OneSignal.init('c1f39bb2-11d8-4ebf-b836-61a0131fb3fa');
+    OneSignal.setAppId('c1f39bb2-11d8-4ebf-b836-61a0131fb3fa');
     this.state = {
       appState: AppState.currentState,
       onRestart: false,
@@ -44,16 +45,34 @@ class App extends Component {
   componentDidMount() {
     AppState.addEventListener('change', this.handleAppStateChange);
     Linking.addEventListener('url', this.handleDeepLink);
-    OneSignal.addEventListener('received', this.onReceived);
-    OneSignal.addEventListener('opened', this.onOpened);
-    OneSignal.addEventListener('ids', this.onIds);
-    OneSignal.inFocusDisplaying(2);
+    OneSignal.promptForPushNotificationsWithUserResponse();
+    OneSignal.setNotificationOpenedHandler((notification) => {
+      console.log('OneSignal: notification opened:', notification);
+    });
+    OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationReceivedEvent) => {
+        console.log(
+          'OneSignal: notification will show in foreground:',
+          notificationReceivedEvent,
+        );
+        let notification = notificationReceivedEvent.getNotification();
+        console.log('notification: ', notification);
+        const data = notification.additionalData;
+        console.log('additionalData: ', data);
+        // Complete with null means don't show a notification.
+        notificationReceivedEvent.complete(notification);
+      },
+    );
+    // OneSignal.addEventListener('received', this.onReceived);
+    // OneSignal.addEventListener('opened', this.onOpened);
+    // OneSignal.addEventListener('ids', this.onIds);
+    // OneSignal.inFocusDisplaying(2);
   }
 
   componentWillUnmount() {
-    OneSignal.removeEventListener('received', this.onReceived);
-    OneSignal.removeEventListener('opened', this.onOpened);
-    OneSignal.removeEventListener('ids', this.onIds);
+    // OneSignal.removeEventListener('received', this.onReceived);
+    // OneSignal.removeEventListener('opened', this.onOpened);
+    // OneSignal.removeEventListener('ids', this.onIds);
     AppState.removeEventListener('change', this.handleAppStateChange);
   }
 
