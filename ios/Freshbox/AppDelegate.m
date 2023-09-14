3e3e2d2f8a841c5c4869a1ea1938bc043277ee8e
Fadly Kayo <fadly.kayo@rebelworks.co>
2023-09-14 17:52:40 +0700

    [iOS] adjust UIViewController

--- ios/Freshbox/AppDelegate.m
+++ ios/Freshbox/AppDelegate.m
@@ -70,6 +70,12 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+	// Place this code after "[self.window makeKeyAndVisible]" and before "return YES;"
+  UIStoryboard *sb = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
+  UIViewController *vc = [sb instantiateInitialViewController];
+  rootView.loadingView = vc.view;
+
   return YES;
 }
 
